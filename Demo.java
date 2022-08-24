977659a8ae3a5d927744db2879aed23db32adcb6
P-HamNguye-N203 <phamnguyen.work@gmail.com>
2022-08-24 20:12:36 +0700

    add

--- Demo.java
+++ Demo.java
@@ -1,4 +1,24 @@
 
 public class Demo {
+	private String ten;
 
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+	
+	public Demo() {
+		
+	}
+	public Demo(String ten) {
+		this.ten = ten;
+	}
+
+	
+	
+	
+	
 }
